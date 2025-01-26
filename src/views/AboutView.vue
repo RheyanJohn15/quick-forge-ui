2de82ae57d79fde617dfec1cada532d19bdc66e2
RheyanJohn15 <rjvblanco.chmsu@gmail.com>
2025-01-27 03:28:34 +0800

    First Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1 class="text-6xl text-red-600">This is an about page</h1>
+    <Button label="Save" :loading="true" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
