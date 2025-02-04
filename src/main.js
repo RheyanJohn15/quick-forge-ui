bab68f3369859fe7a79e5f4835610316fd265f27
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-04 21:52:52 +0800

    Login page added

--- src/main.js
+++ src/main.js
@@ -3,14 +3,55 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
-
+import './assets/main.css'
 const app = createApp(App)
+import { definePreset } from '@primevue/themes';
 
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-      preset: Aura
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      colorScheme: {
+          light: {
+              surface: {
+                  0: '#ffffff',
+                  50: '{green.50}',
+                  100: '{green.100}',
+                  200: '{green.200}',
+                  300: '{green.300}',
+                  400: '{green.400}',
+                  500: '{green.500}',
+                  600: '{green.600}',
+                  700: '{green.700}',
+                  800: '{green.800}',
+                  900: '{green.900}',
+                  950: '{green.950}'
+              }
+          },
+          dark: {
+              surface: {
+                  0: '#ffffff',
+                  50: '{emerald.50}',
+                  100: '{emerald.100}',
+                  200: '{emerald.200}',
+                  300: '{emerald.300}',
+                  400: '{emerald.400}',
+                  500: '{emerald.500}',
+                  600: '{emerald.600}',
+                  700: '{emerald.700}',
+                  800: '{emerald.800}',
+                  900: '{emerald.900}',
+                  950: '{emerald.950}'
+              }
+          }
+      }
   }
 });
 
+app.use(PrimeVue, {
+  theme: {
+      preset: MyPreset
+  }
+});
 app.mount('#app')
+  
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+
+
+<template>
+  <main
+    class="w-full flex-col h-screen bg-gradient-to-r from-green-900 via-green-700 to-green-500 dark:from-emerald-900 dark:via-emerald-700 dark:to-emerald-500 flex items-center justify-center">
+
+    <div class="card bg-white/60 backdrop-blur-3xl p-8 w-1/3 rounded-md">
+     <div class="w-full flex flex-col justify-center items-center">
+      <img :src="'/bg-3.png'" alt="Logo">
+     </div>
+      <div class="flex flex-col mt-8 gap-4 w-full">
+        <div class="flex flex-col gap-2">
+          <label for="username">Username</label>
+          <InputText id="username" placeholder="admin" v-model="value" aria-describedby="username-help" />
+          <Message v-if="false" size="small" severity="error" variant="simple">You must enter a username
+          </Message>
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="password">Password</label>
+          <InputText id="password" placeholder="*********" v-model="value" aria-describedby="password-help" />
+          <Message v-if="false" size="small" severity="error" variant="simple">Enter your username to reset your password.
+          </Message>
+        </div>
+        <Divider />
+        <Button label="Login" />
+      </div>
+    </div>
+  </main>
+</template>
+<script setup>
+import { ref } from "vue";
+</script>
