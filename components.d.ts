08c45f99b1098377c870bfdda8dc732327dd2982
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-06 22:02:30 +0800

    Update UI For Project Content

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Accordion: typeof import('primevue/accordion')['default']
+    AccordionContent: typeof import('primevue/accordioncontent')['default']
+    AccordionHeader: typeof import('primevue/accordionheader')['default']
+    AccordionPanel: typeof import('primevue/accordionpanel')['default']
     Avatar: typeof import('primevue/avatar')['default']
     Button: typeof import('primevue/button')['default']
     Dialog: typeof import('primevue/dialog')['default']
@@ -16,13 +20,19 @@ declare module 'vue' {
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconField: typeof import('primevue/iconfield')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    Image: typeof import('primevue/image')['default']
+    InputIcon: typeof import('primevue/inputicon')['default']
     InputText: typeof import('primevue/inputtext')['default']
+    Menu: typeof import('primevue/menu')['default']
     Menubar: typeof import('primevue/menubar')['default']
     Message: typeof import('primevue/message')['default']
+    Panel: typeof import('primevue/panel')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    SplitButton: typeof import('primevue/splitbutton')['default']
     Step: typeof import('primevue/step')['default']
     StepItem: typeof import('primevue/stepitem')['default']
     StepPanel: typeof import('primevue/steppanel')['default']
@@ -32,6 +42,7 @@ declare module 'vue' {
     Tabs: typeof import('primevue/tabs')['default']
     Textarea: typeof import('primevue/textarea')['default']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
+    Toolbar: typeof import('primevue/toolbar')['default']
     WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
   export interface ComponentCustomProperties {
--- src/views/ProjectContent/Index.vue
+++ src/views/ProjectContent/Index.vue
@@ -1,3 +1,64 @@
 <template>
-  <h1>hello world</h1>
+ <div class="w-full h-screen px-32 text-black py-8 bg-white overflow-auto">
+
+
+  <Panel toggleable>
+    <template #header>
+        <div class="flex p-2 items-center gap-2">
+          <h1 class="text-3xl font-bold">Project Name</h1>
+        </div>
+    </template>
+
+    <template #icons>
+        <Button class="mr-2" text icon="pi pi-pencil"  />
+        <Button icon="pi pi-trash" text severity="danger" />
+    </template>
+    <div class="p-8">
+    <p>Our innovative solutions are designed to streamline operations, enhance efficiency, and drive success. With a focus on quality and reliability, we provide cutting-edge technology tailored to meet your specific needs. Experience seamless integration and exceptional performance with our trusted services</p>
+
+    <Image class="mt-4" src="/1.png" alt="Logo" width="250" preview />
+        <div class="flex gap-2">
+            <Button label="Upload Logo" icon="pi pi-upload" text severity="info" />
+            <Button label="Remove Logo" text icon="pi pi-times" severity="danger" />
+        </div>
+  </div>
+</Panel>
+
+  <Accordion class="mt-4" value="0">
+    <AccordionPanel value="0">
+        <AccordionHeader>Header I</AccordionHeader>
+        <AccordionContent>
+            <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="1">
+        <AccordionHeader>Header II</AccordionHeader>
+        <AccordionContent>
+            <p class="m-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="2">
+        <AccordionHeader>Header III</AccordionHeader>
+        <AccordionContent>
+            <p class="m-0">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
+                qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+</Accordion>
+ </div>
 </template>
+
+<script setup>
+const toggle = (event) => {
+    menu.value.toggle(event);
+};
+
+</script>
