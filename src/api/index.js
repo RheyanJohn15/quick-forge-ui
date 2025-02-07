d63ed7609a08858ce6e7756efced885377a36113
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-07 22:05:13 +0800

    update api getter

--- src/api/index.js
+++ src/api/index.js
@@ -5,29 +5,41 @@ export function useApi() {
 
   const apiToken = localStorage.getItem('api_token') ?? null;
 
+  const headers = (auth) => {
+
+    return {
+      headers: auth ? {
+         Authorization: `Bearer ${apiToken}`,
+        'Content-Type': 'application/json'
+      } : {
+        'Content-Type': 'application/json'
+     }
+    }
+  };
+
   return {
     async get(api, auth = true) {
-      const response = await axios.get(`${apiUrl}${api}`, auth ? { headers: { Authorization: `Bearer ${apiToken}` } }: {});
+      const response = await axios.get(`${apiUrl}${api}`, headers(auth));
       return response.data;
     },
 
     async post(api, data, auth = true) {
-      const response = await axios.post(`${apiUrl}${api}`, data, auth ? { headers: { Authorization: `Bearer ${apiToken}` } } : {});
+      const response = await axios.post(`${apiUrl}${api}`, data, headers(auth));
       return response.data;
     },
 
     async put(api, data, auth = true) {
-      const response = await axios.put(`${apiUrl}${api}`, data, auth ? { headers: { Authorization: `Bearer ${apiToken}` } } : {});
+      const response = await axios.put(`${apiUrl}${api}`, data, headers(auth));
       return response.data;
     },
 
     async patch(api, data, auth = true) {
-      const response = axios.patch(`${apiUrl}${api}`, data, auth ? { headers: { Authorization: `Bearer ${apiToken}` } } : {});
+      const response = axios.patch(`${apiUrl}${api}`, data, headers(auth));
       return response.data;
     },
 
     async delete(api, data, auth = true) {
-      const response = await axios.delete(`${apiUrl}${api}`, data, auth ? { headers: { Authorization: `Bearer ${apiToken}` } } : {});
+      const response = await axios.delete(`${apiUrl}${api}`, data, headers(auth));
       return response.data;
     },
   }
